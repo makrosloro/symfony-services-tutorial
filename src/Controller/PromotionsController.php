c9cee736dd7f8daf73aac05737148d16f2457212
Makros <lorenterodiguezmarcos@gmail.com>
2024-05-20 13:04:23 +0200

    Implements service controller and new custom serializer

---
+++ src/Controller/PromotionsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PromotionsController extends AbstractController
+{
+
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id, DTOSerializer $serializer): Response
+    {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse(
+                ['error' => 'Promotions Engine failure message'],
+                $request->headers->get('force_fail')
+            );
+        }
+
+        //-- 1. Deserialize json data into a EnquiryDTO (Data Transfer Object)
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
+        $lowestPriceEnquiry = $serializer->deserialize(
+            $request->getContent(),
+            LowestPriceEnquiry::class,
+            'json'
+        );
+
+        //-- 2. Pass the Enquiry into a promotions filter
+            //-- The appropiate promotiom will be applied
+
+        //-- 3. Return the modified Enquiry
+        $lowestPriceEnquiry->setDiscountedPrice(50);
+        $lowestPriceEnquiry->setPrice(100);
+        $lowestPriceEnquiry->setPromotionId(3);
+        $lowestPriceEnquiry->setPromotionName('Black Friday half price sale');
+
+        return new JsonResponse($lowestPriceEnquiry, 200);
+
+        /*return new JsonResponse([
+            'quantity' => 5,
+            'request_location' => 'UK',
+            'voucher_code' => 'OU812',
+            'request_date' => '2022-04-04',
+            'product_id' => $id,
+            'price' => 100,
+            'discounted_price' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => ''
+        ], 200);*/
+    }
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions()
+    {
+
+    }
+
+}
