a9ff20cc1c3f587f7cacec3d7eae31808ac880ac
Makros <lorenterodiguezmarcos@gmail.com>
2024-05-21 09:51:16 +0200

    Implements entity attributes loader to read Ignored attributes

--- src/Service/Serializer/DTOSerializer.php
+++ src/Service/Serializer/DTOSerializer.php
@@ -3,6 +3,8 @@
 namespace App\Service\Serializer;
 
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AttributeLoader;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
@@ -16,7 +18,10 @@ class DTOSerializer implements SerializerInterface
     public function __construct()
     {
         $this->serializer = new Serializer(
-            [new ObjectNormalizer(nameConverter: new CamelCaseToSnakeCaseNameConverter())],
+            [new ObjectNormalizer(
+                classMetadataFactory: new ClassMetadataFactory(new AttributeLoader()),
+                nameConverter: new CamelCaseToSnakeCaseNameConverter()
+            )],
             [new JsonEncoder()]
         );
     }
