08c3eb0ad28e01d30ca39146b7d730e93ef1a005
Makros <lorenterodiguezmarcos@gmail.com>
2024-05-22 08:23:31 +0200

    Removes JsonSerializable

--- src/DTO/LowestPriceEnquiry.php
+++ src/DTO/LowestPriceEnquiry.php
@@ -107,10 +107,4 @@ class LowestPriceEnquiry implements PromotionEnquiryInterface
     {
         $this->promotionName = $promotionName;
     }
-
-
-    public function jsonSerialize(): mixed
-    {
-        return get_object_vars($this);
-    }
 }
