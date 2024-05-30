300ca0209169ba576ce8da2306d1b27c6dbe1ca8
Makros <lorenterodiguezmarcos@gmail.com>
2024-05-30 10:55:56 +0200

    Separate methods on interfaces related to promotion and specific to prices

--- src/DTO/PromotionEnquiryInterface.php
+++ src/DTO/PromotionEnquiryInterface.php
@@ -2,7 +2,11 @@
 
 namespace App\DTO;
 
+use App\Entity\Product;
+
 interface PromotionEnquiryInterface
 {
-
+    public function getProduct(): ?Product;
+    public function setPromotionId(int $promotionId);
+    public function setPromotionName(string $promotionName);
 }
