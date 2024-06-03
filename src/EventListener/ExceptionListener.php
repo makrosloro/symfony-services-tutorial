4a6c06ad564f0073df53a4b10972b92860d4358c
Makros <lorenterodiguezmarcos@gmail.com>
2024-06-03 10:55:15 +0200

    First step preparing microservice exception listener

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -2,13 +2,35 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionListener
 {
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
-        dd($exception);
+
+        $response = new JsonResponse([
+            'type' => 'ConstrantException',
+            'title' => 'Exception',
+            'description' => $exception->getMessage(),
+            'violations' => [
+                [
+                    'propertyPath' => 'quantity',
+                    'message' => 'Quantity must be greater than 0.',
+                ],
+            ]
+        ]);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
     }
 }
